54aa589f65b56c609ea5b6585680c317e91a8d1f
victorhponcec <victorhponcec@gmail.com>
2026-02-21 11:58:01 -0300

    test: checkov

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 resource "aws_ssm_parameter" "foo" {
-  #COMMENTEDcheckov:skip=CKV_AWS_337: Using AWS managed key is acceptable for this project
   name  = "foo"
   type  = "SecureString"
   value = "bar"
